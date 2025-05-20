01a1f991b2160d49d5767a6e10c0a8a681ff4661
Gregor Virant <110791157+GregorVirant@users.noreply.github.com>
2025-05-20 12:31:52 +0200

    ROCK-109: Created a drop down menu when clicking on users image in nav bar that lets the user logout or got to his profile.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import HomePage from './components/HomePage'
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
+import ProfilePage from './components/ProfilePage'
 
 const App = () => {
     useEffect(() => {
@@ -23,6 +24,7 @@ const App = () => {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage/>}/>
+                    <Route path="/profile" element={<ProfilePage/>}/>
                 </Routes>
             </main>
         </div>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,9 @@
 // src/components/Header.jsx
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
     const navigate = useNavigate();
 
     const isLoggedIn = (localStorage.getItem("token") != null && localStorage.getItem("user") != null)
@@ -20,15 +21,31 @@ const Header = () => {
 
     return (
         <nav style={{ padding: '10px', backgroundColor: '#f0f0f0', display: 'flex', justifyContent: 'space-around' }}>
+            <a href="/" style={{ textDecoration: 'none', color: 'black' }}>Home</a>
             {isLoggedIn ? 
+                <div style={{ position: 'relative' }}>
+                    <span onClick={() => setShowMenu(!showMenu)}>
+                        {user.username}
+                        <img src={image} alt="Profile picture" width="30" height="30"></img>
+                    </span>
+
+                    {showMenu ? 
+                        <div style={{
+                            position: 'absolute',
+                            top: '100%',
+                            right: 0,
+                            backgroundColor: '#fff',
+                            border: '1px solid #aaa',
+                            padding: '10px',
+                            borderRadius: '4px',
+                            zIndex: 1,
+                        }}>
+                            <button onClick={() => navigate("/profile")} style={{ display: 'block', width: '100%', marginBottom: '5px' }}> Profile </button>
+                            <button onClick={handleLogout} style={{ display: 'block', width: '100%', background: "#faa" }}> Logout</button>
+                        </div>
+                    : ""}
+                </div>:
                 <>
-                    <a href="/" style={{ textDecoration: 'none', color: 'black' }}>Home</a>
-                    <span>User: {user.username}
-                    <img src={image} alt="Profile picture" width="30" height="30"></img></span>
-                    <button onClick={handleLogout}>Logout</button>
-                </>:
-                <>
-                    <a href="/" style={{ textDecoration: 'none', color: 'black' }}>Home</a>
                     <a href="/login" style={{ textDecoration: 'none', color: 'black' }}>Login</a>
                     <a href="/register" style={{ textDecoration: 'none', color: 'black' }}>Register</a>
                 </>
