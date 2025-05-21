feb618aece75754c94c4ed60077f8b16e64e6001
Gregor Virant <110791157+GregorVirant@users.noreply.github.com>
2025-05-21 22:18:49 +0200

    ROCK-147: Added left side bar.

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -44,37 +44,41 @@ const HomePage = () => {
 
     return (
         <div>
-            <h1>Domača stran</h1>
-
-            <input type="text" placeholder="Išči plezališče" value={searchString} onChange={(e) => setSearchString(e.target.value)}/>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>Plezališče</th>
-                        <th>Število poti</th>
-                        <th>Vrste poti</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {filteredAreas.map( (climbingArea, index) => (
-                    <tr key={index}>
-                        <td>{climbingArea.name}</td>
-                        <td>{climbingArea.routes? climbingArea.routes.length : 0}</td>
-                        <td>{(climbingArea.routes && climbingArea.routes.length > 0) ?(
-                            [...new Set(climbingArea.routes.map(route => route.type))].join(","))
-                        :"_"}</td>
-                        
-                        {/*" ("+climbingArea.latitude+ "," + climbingArea.longitude + ")"*/}
-                        
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            {isLoggedIn? <button onClick={()=>{navigate("/addClimbingArea")}}>Add climbing area</button> : ""}
+            <h1>Plezališča v Sloveniji</h1>
+            <div style={{ display: 'flex', height: '70vh' }}>
+                <div style={{width: "200px", maxWidth: "20%", padding: "20px", height: "100%", marginRight: "15px", backgroundColor: "grey"}}> 
+                    <h3>Filtri:</h3>
+                    {isLoggedIn? <button style={{marginTop: "100%"}} onClick={()=>{navigate("/addClimbingArea")}}>Add climbing area</button> : ""}
+                </div>
+                
+                <div style={{flex: 1}}>
+                    <input type="text" placeholder="Išči plezališče" value={searchString} onChange={(e) => setSearchString(e.target.value)}/>            
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Plezališče</th>
+                                <th>Število poti</th>
+                                <th>Vrste poti</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {filteredAreas.map( (climbingArea, index) => (
+                            <tr key={index}>
+                                <td>{climbingArea.name}</td>
+                                <td>{climbingArea.routes? climbingArea.routes.length : 0}</td>
+                                <td>{(climbingArea.routes && climbingArea.routes.length > 0) ?(
+                                    [...new Set(climbingArea.routes.map(route => route.type))].join(","))
+                                :"_"}</td>
+                                
+                                {/*" ("+climbingArea.latitude+ "," + climbingArea.longitude + ")"*/}
+                                
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-        
-        
     );
 };
 
