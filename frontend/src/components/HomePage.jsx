7e1fd30b739b408da1ebf7a6c397c35e340552c0
Gregor Virant <110791157+GregorVirant@users.noreply.github.com>
2025-05-21 14:45:01 +0200

    ROCK-110 getting and showing climbing areas

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
 
 const HomePage = () => {
+    const [error, setError] = useState(null)
+    const [climbingAreas, setClimbingAreas] = useState([])
+
+    useEffect(() => {
+        getClimbingAreas()
+    }, [])
+
+    async function getClimbingAreas(){
+            try {
+                const res = await fetch(`${backendUrl}/climbingAreas`, {
+                    method: "GET",
+                })
+                const data = await res.json()
+                console.log(data)
+
+                if (!res.ok) {
+                    setError("Getting climbing spots failed.")
+                    return
+                }
+                setClimbingAreas(data)
+            }
+            catch (err) {
+                setError("Error getting climbing spots." + err.message)
+            }
+        }
     return (
-        <p>"Home page"</p>
+        <div>
+            <h1>Home page</h1>
+            <ul>
+                {climbingAreas.map( (climbingArea, index) => (
+                    <li key={index}>
+                        {climbingArea.name} 
+                        {" ("+climbingArea.latitude+ "," + climbingArea.longitude + ")"}
+                    </li>
+                ))}
+            </ul>
+        </div>
+        
+        
     );
 };
 
