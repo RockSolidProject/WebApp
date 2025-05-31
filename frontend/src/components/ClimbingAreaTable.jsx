e6c684a3d7c2faea00ea24de1b8f1eb941b4a95b
Gregor Virant <110791157+GregorVirant@users.noreply.github.com>
2025-05-31 21:46:50 +0200

    ROCK-167 Made the tables at the bottom of th hompage look better

--- frontend/src/components/ClimbingAreaTable.jsx
+++ frontend/src/components/ClimbingAreaTable.jsx
@@ -1,36 +1,57 @@
 import React, { useState } from 'react';
+import {
+  Table, TableHead, TableBody, TableRow, TableCell,
+  Typography,Collapse, Box, 
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 const ClimbingAreaTable = ({ filteredAreas }) => {
-    const [isTableVisible, setIsTableVisible] = useState(false);
+    const [isTableVisible, setIsTableVisible] = useState(true);
 
     return (
-        <div>
-            <button onClick={() => setIsTableVisible(!isTableVisible)}>
-                {isTableVisible ? "Skrij zunanja plezališča" : "Pokaži zunanja plezališča"}
-            </button>
-            {isTableVisible && (
-                <table className="tabela">
-                    <thead>
-                    <tr>
-                        <th>Plezališče</th>
-                        <th>Število poti</th>
-                        <th>Vrste poti</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {filteredAreas.map((climbingArea, index) => (
-                        <tr key={index}>
-                            <td>{climbingArea.name}</td>
-                            <td>{climbingArea.routes ? climbingArea.routes.length : 0}</td>
-                            <td>{(climbingArea.routes && climbingArea.routes.length > 0) ? (
-                                [...new Set(climbingArea.routes.map(route => route.type))].join(",")
-                            ) : "_"}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            )}
-        </div>
+        <Box sx={{ width: '100%' }}>
+            <Box 
+                onClick={() => setIsTableVisible(!isTableVisible)}
+                sx={{
+                    display: "flex",
+                    alignItems: 'center',
+                    cursor: 'pointer',
+                    mb: 1,
+                    '&:hover': { color: "#1565c0" }
+                }}
+            >
+                {isTableVisible ? <ExpandMoreIcon/>: <ChevronRightIcon/>}
+                <Typography variant="h5" sx={{userSelect: 'none'}}>
+                    Zunanja plezališča
+                </Typography>
+            </Box>
+
+            <Collapse in={isTableVisible}>
+                <Box>
+                    <Table size="small">
+                        <TableHead>
+                            <TableRow sx={{ bgcolor: 'grey.100' }}>
+                                <TableCell><Typography fontWeight="550">Plezališče</Typography></TableCell>
+                                <TableCell align='center'><Typography fontWeight="550">Število poti</Typography></TableCell>
+                                <TableCell align='center'><Typography fontWeight="550">Vrste poti</Typography></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {filteredAreas.map((climbingArea, index) => (
+                            <TableRow key={index}>
+                                <TableCell>{climbingArea.name}</TableCell>
+                                <TableCell align='center'>{climbingArea.routes ? climbingArea.routes.length : 0}</TableCell>
+                                <TableCell align='center'>{(climbingArea.routes && climbingArea.routes.length > 0) ? (
+                                    [...new Set(climbingArea.routes.map(route => route.type))].join(",")
+                                ) : "_"}</TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </Box>
+            </Collapse>
+        </Box>
     );
 };
 
