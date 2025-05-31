9089566e32623f719ab13cae63de372a62d428ac
Gregor Virant <110791157+GregorVirant@users.noreply.github.com>
2025-05-31 21:57:02 +0200

    ROCK-167: Bottom tables disapear if they have no data.

--- frontend/src/components/ClimbingAreaTable.jsx
+++ frontend/src/components/ClimbingAreaTable.jsx
@@ -9,49 +9,53 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 const ClimbingAreaTable = ({ filteredAreas }) => {
     const [isTableVisible, setIsTableVisible] = useState(true);
 
-    return (
-        <Box sx={{ width: '100%' }}>
-            <Box 
-                onClick={() => setIsTableVisible(!isTableVisible)}
-                sx={{
-                    display: "flex",
-                    alignItems: 'center',
-                    cursor: 'pointer',
-                    mb: 1,
-                    '&:hover': { color: "#1565c0" }
-                }}
-            >
-                {isTableVisible ? <ExpandMoreIcon/>: <ChevronRightIcon/>}
-                <Typography variant="h5" sx={{userSelect: 'none'}}>
-                    Zunanja plezališča
-                </Typography>
-            </Box>
+    const hasData = filteredAreas.length > 0
 
-            <Collapse in={isTableVisible}>
-                <Box>
-                    <Table size="small">
-                        <TableHead>
-                            <TableRow sx={{ bgcolor: 'grey.100' }}>
-                                <TableCell><Typography fontWeight="550">Plezališče</Typography></TableCell>
-                                <TableCell align='center'><Typography fontWeight="550">Število poti</Typography></TableCell>
-                                <TableCell align='center'><Typography fontWeight="550">Vrste poti</Typography></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                        {filteredAreas.map((climbingArea, index) => (
-                            <TableRow key={index}>
-                                <TableCell>{climbingArea.name}</TableCell>
-                                <TableCell align='center'>{climbingArea.routes ? climbingArea.routes.length : 0}</TableCell>
-                                <TableCell align='center'>{(climbingArea.routes && climbingArea.routes.length > 0) ? (
-                                    [...new Set(climbingArea.routes.map(route => route.type))].join(",")
-                                ) : "_"}</TableCell>
-                            </TableRow>
-                        ))}
-                        </TableBody>
-                    </Table>
+    return (
+        <>{hasData && (
+            <Box sx={{ width: '100%' }}>
+                <Box 
+                    onClick={() => setIsTableVisible(!isTableVisible)}
+                    sx={{
+                        display: "flex",
+                        alignItems: 'center',
+                        cursor: 'pointer',
+                        mb: 1,
+                        '&:hover': { color: "#1565c0" }
+                    }}
+                >
+                    {isTableVisible ? <ExpandMoreIcon/>: <ChevronRightIcon/>}
+                    <Typography variant="h5" sx={{userSelect: 'none'}}>
+                        Zunanja plezališča
+                    </Typography>
                 </Box>
-            </Collapse>
-        </Box>
+
+                <Collapse in={isTableVisible}>
+                    <Box>
+                        <Table size="small">
+                            <TableHead>
+                                <TableRow sx={{ bgcolor: 'grey.100' }}>
+                                    <TableCell><Typography fontWeight="550">Plezališče</Typography></TableCell>
+                                    <TableCell align='center'><Typography fontWeight="550">Število poti</Typography></TableCell>
+                                    <TableCell align='center'><Typography fontWeight="550">Vrste poti</Typography></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {filteredAreas.map((climbingArea, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>{climbingArea.name}</TableCell>
+                                    <TableCell align='center'>{climbingArea.routes ? climbingArea.routes.length : 0}</TableCell>
+                                    <TableCell align='center'>{(climbingArea.routes && climbingArea.routes.length > 0) ? (
+                                        [...new Set(climbingArea.routes.map(route => route.type))].join(",")
+                                    ) : "_"}</TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </Box>
+                </Collapse>
+            </Box>
+        )}</>
     );
 };
 
